a1dde838da1b0d2d57be86ffb01dcfc7fac26699
Veellert <vee9040@gmail.com>
2020-11-06 19:10:31 +1000

    todo

--- todo/redtodo.php
+++
@@ -1,8 +0,0 @@
-<?php
-require 'dbconf.php';
-$id = $_GET['id'];
-$task = $_GET['task'];
-$sql = "UPDATE `tasks` SET `task` = '$task' WHERE `id` = ?";
-$query = $pdo->prepare($sql);
-$query->execute([$id]);
-header('Location: ./index.php');
